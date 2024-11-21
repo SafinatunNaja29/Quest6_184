1c9588f90de6dca1c328b1b7543d7b30a076d528
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-11-21 18:42:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigationcompose/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.navigationcompose.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.navigation.compose.composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationcompose.model.Mahasiswa
+import com.example.navigationcompose.ui.view.screen.SplashView
+import com.example.navigationcompose.ui.view.viewmodel.MahasiswaViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    navController: NavController = rememberNavController()
+){
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = modifier.padding()
+    ) {
+        composable (route = Halaman.Splash.name){
+            SplashView (onMulaiButton = {
+                navController.navigate()
+            } ) { }
+        }
+    }
+}
