4fd955efbdcf25cce1b23853b42b64fa133e0a3b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-11-22 12:44:28 +0700

    menambahkan variable dalam class RencanaStudyViewModel

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.navigationcompose.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.navigationcompose.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
 
 }
