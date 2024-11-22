4c862543f39c1bf6690b6f9856d157d9e4331307
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-11-22 10:03:57 +0700

    membuat object matakuliah dengan beberapa daftar mata kuliah

--- app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.navigationcompose.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrogaman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
---
+++ app/src/main/java/com/example/navigationcompose/widget/DynamicSelectedField.kt
@@ -0,0 +1,4 @@
+package com.example.navigationcompose.widget
+
+class DynamicSelectedField {
+}
