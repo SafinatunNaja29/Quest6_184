1a06e0d71be1b17a27d884f84f32a9d7ada01227
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-11-22 11:01:49 +0700

    membuat kolom dengan background color primary

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,19 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -16,4 +25,12 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column (
+        modifier = Modifier
+            . fillMaxWidth()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+
+    }
 }
