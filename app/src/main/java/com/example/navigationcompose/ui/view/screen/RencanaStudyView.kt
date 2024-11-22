b992b20891bfd4dbd5e4c3c62f038f13a0af9a81
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-11-22 10:49:58 +0700

    menambahkan parameter dalam fungsi RencanaStudyView

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,8 +1,11 @@
 package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
 fun RencanaStudyView(
-
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
 ){}
